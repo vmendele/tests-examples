58ccb18c094d5a3ee4c7cd65450767447c1a8cf4
Victor <victor.mendele68@gmail.com>
2025-06-19 16:21:15 +0200

    add comment

--- src/divide.ts
+++ src/divide.ts
@@ -1,3 +1,4 @@
+// Voici la fonction a testé contenant deux chemins de code.
 export function divide(x: number, y: number): number {
     if (x === 0 || y === 0)
         throw new Error("Cannot divide by 0")
